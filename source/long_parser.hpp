49a720a97aff63fab171818e604c16c3155eb25b
octopus-prime <mike_gresens@notebook>
2016-02-18 20:50:45 +0100

    Worked on long_parser.

--- source/long_parser.hpp
+++ source/long_parser.hpp
@@ -21,50 +21,19 @@ typedef std::int64_t long_t;
 namespace parser {
 namespace detail {
 
-//long ::= 'L' b7 b6 b5 b4 b3 b2 b1 b0
-//     ::= [xd8-xef]
-//     ::= [xf0-xff] b0
-//     ::= [x38-x3f] b1 b0
-//     ::= x77 b3 b2 b1 b0
-
-const auto long1_action = [](auto& ctx)
-{
-	// code - xe0
-	const long_t b0 = x3::_attr(ctx);
-	x3::_val(ctx) = b0 - 0xe0;
-};
-
-const auto long2_action = [](auto& ctx)
-{
-	// 256 * (code - xf8) + b0
-	using boost::fusion::at_c;
-	const long_t b0 = at_c<1>(x3::_attr(ctx));
-	const long_t b1 = at_c<0>(x3::_attr(ctx));
-	x3::_val(ctx) = ((b1 - 0xf8) << 8) + b0;
-};
-
-const auto long3_action = [](auto& ctx)
-{
-	// 65536 * (code - x3c) + 256 * b1 + b0
-	using boost::fusion::at_c;
-	const long_t b0 = at_c<1>(x3::_attr(ctx));
-	const long_t b1 = at_c<0>(x3::_attr(ctx));
-	x3::_val(ctx) = ((b1 - 0x3c) << 16) + b0;
-};
-
 const x3::rule<class long_rule, long_t> long_rule("long");
 const x3::rule<class long1_rule, long_t> long1_rule;
 const x3::rule<class long2_rule, long_t> long2_rule;
 const x3::rule<class long3_rule, long_t> long3_rule;
-const x3::rule<class long4_rule, long_t> long4_rule;
+const x3::rule<class long4_rule, std::int32_t> long4_rule;
 const x3::rule<class long5_rule, long_t> long5_rule;
 
 const auto long_rule_def = long1_rule | long2_rule | long3_rule | long4_rule | long5_rule;
 
-const auto long1_rule_def = byte_rule(0xd8, 0xef) [ long1_action ];
-const auto long2_rule_def = (byte_rule(0xf0, 0xff) >> x3::byte_) [ long2_action ];
-const auto long3_rule_def = (byte_rule(0x38, 0x3f) >> x3::big_word) [ long3_action ];
-const auto long4_rule_def = x3::lit('w') >> x3::big_dword;
+const auto long1_rule_def = byte_rule(0xd8, 0xef) [ calc_action<long_t,0>(0xe0) ];
+const auto long2_rule_def = (byte_rule(0xf0, 0xff) >> x3::byte_) [ calc_action<long_t,8>(0xf8) ];
+const auto long3_rule_def = (byte_rule(0x38, 0x3f) >> x3::big_word) [ calc_action<long_t,16>(0x3c) ];
+const auto long4_rule_def = x3::lit('Y') >> x3::big_dword;
 const auto long5_rule_def = x3::lit('L') >> x3::big_qword;
 
 BOOST_SPIRIT_DEFINE(long_rule, long1_rule, long2_rule, long3_rule, long4_rule, long5_rule);
--- test/test_long.cpp
+++ test/test_long.cpp
@@ -10,48 +10,29 @@
 #include <boost/test/data/test_case.hpp>
 #include "sample.hpp"
 
-using namespace std::literals;
-
 namespace hessian {
 namespace parser {
 
-//xe0                  # 0
-//xd8                  # -8
-//xef                  # 15
-//
-//xf8 x00              # 0
-//xf0 x00              # -2048
-//xf7 x00              # -256
-//xff xff              # 2047
-//
-//x3c x00 x00          # 0
-//x38 x00 x00          # -262144
-//x3f xff xff          # 262143
-//
-//x77 x00 x00 x00 x00  # 0
-//x77 x00 x00 x01 x2c  # 300
-//
-//L x00 x00 x00 x00 x00 x00 x01 x2c  # 300
-
 const samples_t<long_t> samples
 {
-	{"\xe0"s, 0},
-	{"\xd8"s, -8},
-	{"\xef"s, 15},
-
-	{"\xf8\x00"s, 0},
-	{"\xf0\x00"s, -2048},
-	{"\xf7\x00"s, -256},
-	{"\xff\xff"s, 2047},
-
-	{"\x3c\x00\x00"s, 0},
-	{"\x38\x00\x00"s, -262144},
-	{"\x3f\xff\xff"s, 262143},
-
-	{"w\x00\x00\x00\x00"s, 0},
-	{"w\x00\x00\x01\x2c"s, 300},
-
-	{"L\x00\x00\x00\x00\x00\x00\x01\x2c"s, 300}
+	{"\xe0"s, 0l},
+	{"\xe1"s, 1l},
+	{"\xef"s, 15l},
+	{"\xd8"s, -8l},
+	{"\xf8\x10"s, 0x10l},
+	{"\xff\xff"s, 0x7ffl},
+	{"\xf7\xf7"s, -9l},
+	{"\xf0\x00"s, -0x800l},
+	{"\x3c\x08\x00"s, 0x800l},
+	{"\x3b\xf7\xff"s, -0x801l},
+	{"\x3f\xff\xff"s, 0x3ffffl},
+	{"\x38\x00\x00"s, -0x40000l},
+	{"Y\x00\x04\x00\x00"s, 0x40000l},
+	{"Y\xff\xfb\xff\xff"s, -0x40001l},
+	{"Y\x7f\xff\xff\xff"s, 0x7fffffffl},
+	{"Y\x80\x00\x00\x00"s, -0x80000000l},
+	{"L\x00\x00\x00\x00\x80\x00\x00\x00"s, 0x80000000l},
+	{"L\xff\xff\xff\xff\x7f\xff\xff\xff"s, -0x80000001l}
 };
 
 BOOST_AUTO_TEST_SUITE(test_long)
@@ -71,9 +52,8 @@ BOOST_AUTO_TEST_CASE(test_failure)
 {
 	const std::string text("x");
 	long_t attr;
-	auto iter = text.begin();
 
-	BOOST_CHECK(!x3::parse(iter, text.end(), long_rule, attr));
+	BOOST_CHECK(!x3::parse(text.begin(), text.end(), long_rule, attr));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
