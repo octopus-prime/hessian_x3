e7cf5b4bd1075eb98406305701d62cc2b0cfeacb
octopus-prime <mike_gresens@notebook>
2016-02-18 20:50:11 +0100

    Worked on int_parser.

--- source/int_parser.hpp
+++ source/int_parser.hpp
@@ -21,35 +21,6 @@ typedef std::int32_t int_t;
 namespace parser {
 namespace detail {
 
-//int ::= 'I' b3 b2 b1 b0
-//    ::= [x80-xbf]
-//    ::= [xc0-xcf] b0
-//    ::= [xd0-xd7] b1 b0
-
-const auto int1_action = [](auto& ctx)
-{
-	const int_t b0 = x3::_attr(ctx);
-	x3::_val(ctx) = b0 - 0x90;
-};
-
-const auto int2_action = [](auto& ctx)
-{
-	// 256 * (code - xc8) + b0
-	using boost::fusion::at_c;
-	const int_t b0 = at_c<1>(x3::_attr(ctx));
-	const int_t b1 = at_c<0>(x3::_attr(ctx));
-	x3::_val(ctx) = ((b1 - 0xc8) << 8) + b0;
-};
-
-const auto int3_action = [](auto& ctx)
-{
-	// 65536 * (code - xd4) + 256 * b1 + b0
-	using boost::fusion::at_c;
-	const int_t b0 = at_c<1>(x3::_attr(ctx));
-	const int_t b1 = at_c<0>(x3::_attr(ctx));
-	x3::_val(ctx) = ((b1 - 0xd4) << 16) + b0;
-};
-
 const x3::rule<class int_rule, int_t> int_rule("int");
 const x3::rule<class int1_rule, int_t> int1_rule;
 const x3::rule<class int2_rule, int_t> int2_rule;
@@ -57,9 +28,9 @@ const x3::rule<class int3_rule, int_t> int3_rule;
 const x3::rule<class int4_rule, int_t> int4_rule;
 
 const auto int_rule_def = int1_rule | int2_rule | int3_rule | int4_rule;
-const auto int1_rule_def = byte_rule(0x80, 0xbf) [ int1_action ];
-const auto int2_rule_def = (byte_rule(0xc0, 0xcf) >> x3::byte_) [ int2_action ];
-const auto int3_rule_def = (byte_rule(0xd0, 0xdf) >> x3::big_word) [ int3_action ];
+const auto int1_rule_def = byte_rule(0x80, 0xbf) [ calc_action<int_t,0>(0x90) ];
+const auto int2_rule_def = (byte_rule(0xc0, 0xcf) >> x3::byte_) [ calc_action<int_t,8>(0xc8) ];
+const auto int3_rule_def = (byte_rule(0xd0, 0xdf) >> x3::big_word) [ calc_action<int_t,16>(0xd4) ];
 const auto int4_rule_def = x3::lit('I') >> x3::big_dword;
 
 BOOST_SPIRIT_DEFINE(int_rule, int1_rule, int2_rule, int3_rule, int4_rule);
--- test/test_int.cpp
+++ test/test_int.cpp
@@ -10,44 +10,27 @@
 #include <boost/test/data/test_case.hpp>
 #include "sample.hpp"
 
-using namespace std::literals;
-
 namespace hessian {
 namespace parser {
-
-//		x90                # 0
-//		x80                # -16
-//		xbf                # 47
-//
-//		xc8 x00            # 0
-//		xc0 x00            # -2048
-//		xc7 x00            # -256
-//		xcf xff            # 2047
-//
-//		xd4 x00 x00        # 0
-//		xd0 x00 x00        # -262144
-//		xd7 xff xff        # 262143
-//
-//		I x00 x00 x00 x00  # 0
-//		I x00 x00 x01 x2c  # 300
 
 const samples_t<int_t> samples
 {
 	{"\x90"s, 0},
-	{"\x80"s, -16},
+	{"\x91"s, 1},
 	{"\xbf"s, 47},
-
-	{"\xc8\x00"s, 0},
-	{"\xc0\x00"s, -2048},
-	{"\xc7\x00"s, -256},
-	{"\xcf\xff"s, 2047},
-
-	{"\xd4\x00\x00"s, 0},
-	{"\xd0\x00\x00"s, -262144},
-	{"\xd7\xff\xff"s, 262143},
-
-	{"I\x00\x00\x00\x00"s, 0},
-	{"I\x00\x00\x01\x2c"s, 300}
+	{"\x80"s, -16},
+	{"\xc8\x30"s, 0x30},
+	{"\xcf\xff"s, 0x7ff},
+	{"\xc7\xef"s, -17},
+	{"\xc0\x00"s, -0x800},
+	{"\xd4\x08\x00"s, 0x800},
+	{"\xd7\xff\xff"s, 0x3ffff},
+	{"\xd3\xf7\xff"s, -0x801},
+	{"\xd0\x00\x00"s, -0x40000},
+	{"I\x00\x04\x00\x00"s, 0x40000},
+	{"I\x7f\xff\xff\xff"s, 0x7fffffff},
+	{"I\xff\xfb\xff\xff"s, -0x40001},
+	{"I\x80\x00\x00\x00"s, -0x80000000}
 };
 
 BOOST_AUTO_TEST_SUITE(test_int)
@@ -67,9 +50,8 @@ BOOST_AUTO_TEST_CASE(test_failure)
 {
 	const std::string text("x");
 	int_t attr;
-	auto iter = text.begin();
 
-	BOOST_CHECK(!x3::parse(iter, text.end(), int_rule, attr));
+	BOOST_CHECK(!x3::parse(text.begin(), text.end(), int_rule, attr));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
