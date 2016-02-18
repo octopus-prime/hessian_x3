fb224e1609bc4551e810c0809ea10f4c342c4302
octopus-prime <mike_gresens@notebook>
2016-02-18 19:50:26 +0100

    Worked on double_parser.

--- test/test_double.cpp
+++ test/test_double.cpp
@@ -10,38 +10,24 @@
 #include <boost/test/data/test_case.hpp>
 #include "sample.hpp"
 
-using namespace std::literals;
-
 namespace hessian {
 namespace parser {
 
-//x67          # 0.0
-//x68          # 1.0
-//
-//x69 x00      # 0.0
-//x69 x80      # -128.0
-//x69 xff      # 127.0
-//
-//x70 x00 x00  # 0.0
-//x70 x80 x00  # -32768.0
-//x70 xff xff  # 32767.0
-//
-//D x40 x28 x80 x00 x00 x00 x00 x00  # 12.25
-
 const samples_t<double_t> samples
 {
-	{"\x67"s, 0.0},
-	{"\x68"s, 1.0},
-
-	{"\x69\x00"s, 0.0},
-	{"\x69\x80"s, -128.0},
-	{"\x69\xff"s, 127.0},
-
-	{"\x6a\x00\x00"s, 0.0},
-	{"\x6a\x80\x00"s, -32768.0},
-	{"\x6a\xff\xff"s, 32767.0},
-
-	{"D\x40\x28\x80\x00\x00\x00\x00\x00"s, 12.25}
+	{"\x5b"s, 0.0},
+	{"\x5c"s, 1.0},
+	{"\x5d\x02"s, 2.0},
+	{"\x5d\x7f"s, 127.0},
+	{"\x5d\x80"s, -128.0},
+	{"\x5e\x00\x80"s, 128.0},
+	{"\x5e\xff\x7f"s, -129.0},
+	{"\x5e\x7f\xff"s, 32767.0},
+	{"\x5e\x80\x00"s, -32768.0},
+	{"\x5f\x00\x00\x00\x01"s, 0.001},
+	{"\x5f\xff\xff\xff\xff"s, -0.001},
+	{"\x5f\x00\x01\x00\x00"s, 65.536},
+	{"D\x40\x09\x21\xf9\xf0\x1b\x86\x6e"s, 3.14159}
 };
 
 BOOST_AUTO_TEST_SUITE(test_double)
@@ -61,9 +47,8 @@ BOOST_AUTO_TEST_CASE(test_failure)
 {
 	const std::string text("x");
 	double_t attr;
-	auto iter = text.begin();
 
-	BOOST_CHECK(!x3::parse(iter, text.end(), double_rule, attr));
+	BOOST_CHECK(!x3::parse(text.begin(), text.end(), double_rule, attr));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
