713c2d60b8e0ec88dba802ed8db8ce0b466b6cd9
octopus-prime <mike_gresens@notebook>
2016-02-18 20:49:25 +0100

    Added calc_action

--- source/byte_parser.hpp
+++ source/byte_parser.hpp
@@ -31,6 +31,40 @@ const auto byte_rule = [](const byte_t from, const byte_t to)
 	return x3::byte_ [check];
 };
 
+template <typename T, size_t N>
+class calc_action
+{
+public:
+	calc_action(const T offset) : _offset(offset) {}
+
+	void operator()(auto& ctx) const
+	{
+		using boost::fusion::at_c;
+		const T b1 = at_c<0>(x3::_attr(ctx));
+		const T b0 = at_c<1>(x3::_attr(ctx));
+		x3::_val(ctx) = ((b1 - _offset) << N) + b0;
+	};
+
+private:
+	T _offset;
+};
+
+template <typename T>
+struct calc_action<T, 0>
+{
+public:
+	calc_action(const T offset) : _offset(offset) {}
+
+	void operator()(auto& ctx) const
+	{
+		const T b0 = x3::_attr(ctx);
+		x3::_val(ctx) = b0 - _offset;
+	}
+
+private:
+	T _offset;
+};
+
 }
 }
 }
