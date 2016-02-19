a5d995d6fbf836809bcf70b385784559fff4e07e
octopus-prime <mike_gresens@notebook>
2016-02-19 19:44:15 +0100

    Worked on list parser.

--- source/parser/parser_list.hpp
+++ source/parser/parser_list.hpp
@@ -16,6 +16,8 @@ const x3::rule<class list2_rule, list_t> list2_rule;
 const x3::rule<class length_rule, size_t> length_rule("length");
 const x3::rule<class length1_rule, size_t> length1_rule;
 const x3::rule<class length2_rule, size_t> length2_rule;
+const x3::rule<class length3_rule, size_t> length3_rule;
+const x3::rule<class length4_rule, size_t> length4_rule;
 
 struct list_parser : x3::parser<list_parser>
 {
@@ -27,57 +29,25 @@ struct list_parser : x3::parser<list_parser>
 		const auto saved = f;
 		size_t len;
 
-		if (x3::parse(f,l,length_rule,len))
-		{
-			size_t i = 0;
-
-			for (; i < len; ++i)
-			{
-				value_t val;
-
-				if (!x3::parse(f, l, value_rule, val))
-					break;
-
-				attr.push_back(val);
-			}
-
-			return len == i;
-		}
+		if (x3::parse(f, l, length_rule, len))
+			if (x3::parse(f, l, x3::repeat(len) [value_rule], attr))
+				return true;
 
 		f = saved;
 		return false;
 	}
 };
 
-const auto list_action = [](auto& ctx)
-{
-	const size_t length = x3::_attr(ctx);
-	list_t list;
-
-	auto begin = x3::_where(ctx).begin();
-	auto end = x3::_where(ctx).end();
-
-	std::cout << length << std::endl;
-	for (size_t i = 0; i < length; ++i)
-	{
-		std::cout << i << std::endl;
-		value_t value;
-		x3::parse(begin, end, value_rule, value);
-		list.push_back(value);
-	}
-
-	x3::_val(ctx) = std::move(list);
-};
-
 const auto list_rule_def = list1_rule | list2_rule;
 const auto list1_rule_def = x3::lit('W') >> *(value_rule - x3::lit('Z')) >> x3::lit('Z');
-//const auto list2_rule_def = list_parser();
-const auto list2_rule_def = length_rule [ list_action ];
-const auto length_rule_def = length1_rule | length2_rule;
+const auto list2_rule_def = list_parser();
+const auto length_rule_def = length1_rule | length2_rule | length3_rule | length4_rule;
 const auto length1_rule_def = byte_rule(0x78, 0x7f) [ calc_action<size_t,0>(0x78) ];
 const auto length2_rule_def = x3::lit('X') >> int_rule;
+const auto length3_rule_def = (byte_rule(0x70, 0x77) >> type_rule) [ calc_action<size_t,0>(0x70) ];
+const auto length4_rule_def = x3::lit('V') >> type_rule >> int_rule;
 
-BOOST_SPIRIT_DEFINE(list_rule, list1_rule, list2_rule, length_rule, length1_rule, length2_rule);
+BOOST_SPIRIT_DEFINE(list_rule, list1_rule, list2_rule, length_rule, length1_rule, length2_rule, length3_rule, length4_rule);
 
 }
 }
