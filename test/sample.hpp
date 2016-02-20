c1b113b9e5841afa3fbffcbcea51da5060b6d11a
octopus-prime <mike_gresens@notebook>
2016-02-20 13:56:51 +0100

    Minor

--- test/sample.hpp
+++ test/sample.hpp
@@ -12,17 +12,21 @@
 
 using namespace std::literals;
 
+namespace hessian {
+
 template <typename T>
 using sample_t = std::pair<std::string, T>;
 
 template <typename T>
 using samples_t = std::vector<sample_t<T>>;
 
+}
+
 namespace std {
 
 template <typename T>
 inline ostream&
-operator<<(ostream& stream, const sample_t<T>& sample)
+operator<<(ostream& stream, const hessian::sample_t<T>& sample)
 {
 	stream << boost::algorithm::hex(sample.first) << " & " << hessian::value_t(sample.second);
 	return stream;
