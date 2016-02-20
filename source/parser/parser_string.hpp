83c72e6ae66131bc914f3dab47a585055b976f7b
octopus-prime <mike_gresens@notebook>
2016-02-20 13:56:33 +0100

    Worked on string

--- source/parser/parser_string.hpp
+++ source/parser/parser_string.hpp
@@ -11,14 +11,14 @@
 
 namespace hessian {
 namespace parser {
-
-//string ::= s b1 b0 <utf8-data> string
-//       ::= S b1 b0 <utf8-data>
-//       ::= [x00-x1f] <utf8-data>
-
-// NOTE: The length means number of UTF16 characters but the content is given in UTF8 characters!
+namespace string {
 
 const x3::rule<class string_rule, string_t> string_rule("string");
+const x3::rule<class length_rule, std::tuple<size_t,bool>> length_rule;
+const x3::rule<class length1_rule, size_t> length1_rule;
+const x3::rule<class length2_rule, size_t> length2_rule;
+const x3::rule<class length3_rule, size_t> length3_rule;
+const x3::rule<class length4_rule, size_t> length4_rule;
 
 struct string_parser : x3::parser<string_parser>
 {
@@ -31,106 +31,42 @@ struct string_parser : x3::parser<string_parser>
 	bool parse(It& f, It const& l, Ctx&, x3::unused_type, Attr& attr) const
     {
 		const auto saved = f;
-		char type = 'S';
 		size_t len;
-		auto tied = std::tie(type, len);
+		bool done;
+		auto tied = std::tie(len, done);
 
-//		const u8_u16_iterator<It> e(l);
-		while (x3::parse(f,l,x3::char_("sS") >> x3::big_word,tied) || x3::parse(f,l,byte_rule(0x00, 0x1f), len))
+		while (x3::parse(f,l,length_rule,tied))
 		{
+			// NOTE: The length means number of UTF16 characters but the content is given in UTF8 characters!
 			u8_u16_iterator<It> i(f);
-			// std::advance(i, len); // broken
-			std::__advance(i, len, std::input_iterator_tag());
+			std::__advance(i, len, std::input_iterator_tag()); //NOTE: std::advance(i, len); broken here
 
 			const auto s = f;
 			f = i.base().base();
 
-			attr.insert(attr.cend(), s, f);
+			x3::traits::append(attr, s, f);
 
-			if (type == 'S')
+			if (done)
 				return true;
-
-			type = 'S';
-		}
-
-		f = saved;
-		return false;
-	}
-
-    template <typename It, typename Ctx>
-	bool parse(It& f, It const& l, Ctx&, x3::unused_type, x3::unused_type) const
-    {
-		const auto saved = f;
-		char type = 'S';
-		size_t len;
-		auto tied = std::tie(type, len);
-
-//		const u8_u16_iterator<It> e(l);
-		while (x3::parse(f,l,x3::char_("sS") >> x3::big_word,tied) || x3::parse(f,l,byte_rule(0x00, 0x1f), len))
-		{
-			u8_u16_iterator<It> i(f);
-			// std::advance(i, len); // broken
-			std::__advance(i, len, std::input_iterator_tag());
-
-			const auto s = f;
-			f = i.base().base();
-
-			if (type == 'S')
-				return true;
-
-			type = 'S';
 		}
 
 		f = saved;
 		return false;
 	}
 };
-
-//std::string what(string_parser const& p)
-//{
-//    return "string";
-//}
-
-/*
-struct length_tag;
-const auto length_action = [](auto& ctx)
-{
-	*x3::get<length_tag>(ctx) = x3::_attr(ctx);
-};
-const auto more_action = [](auto &ctx)
-{
-	x3::_pass(ctx) = *x3::get<length_tag>(ctx) >  x3::_val(ctx).size();
-};
-const auto done_action = [](auto &ctx)
-{
-	x3::_pass(ctx) = *x3::get<length_tag>(ctx) == x3::_val(ctx).size();
-};
-const auto push_action = [](auto &ctx)
-{
-	x3::_val(ctx).push_back(x3::_attr(ctx));
-};
-
-const x3::rule<class string_rule, string_t> string_rule("string");
-const x3::rule<class string1_rule, string_t> string1_rule;
-const x3::rule<class string2_rule, string_t> string2_rule;
-
-const auto length_rule = x3::omit[x3::big_word [length_action] ];
-const auto content_rule = x3::lexeme[ *(x3::eps [more_action] >> x3::char_ [push_action]) >> x3::eps [done_action] ];
-
-const auto string_rule_def = x3::with<length_tag>(std::make_shared<std::size_t>())[string1_rule | string2_rule];
-const auto string1_rule_def = x3::lit('S') >> length_rule >> content_rule;
-const auto string2_rule_def = x3::lit('s') >> length_rule >> content_rule >> string_rule;
-
-BOOST_SPIRIT_DEFINE(string_rule, string1_rule, string2_rule);
-*/
-
-//const x3::rule<class string_rule, string_t> string_rule("string");
-//const auto string_rule_def = bstring();
-//BOOST_SPIRIT_DEFINE(string_rule);
 
 const auto string_rule_def = string_parser();
+const auto length_rule_def = ((length1_rule | length2_rule | length3_rule) >> x3::attr(true)) | (length4_rule >> x3::attr(false));
+const auto length1_rule_def = byte_rule(0x00, 0x1f);
+const auto length2_rule_def = (byte_rule(0x30, 0x33) >> x3::byte_) [ calc_action<size_t,8>(0x30) ];
+const auto length3_rule_def = x3::lit("S") >> x3::big_word;
+const auto length4_rule_def = x3::lit("R") >> x3::big_word;
 
-BOOST_SPIRIT_DEFINE(string_rule);
+BOOST_SPIRIT_DEFINE(string_rule, length_rule, length1_rule, length2_rule, length3_rule, length4_rule);
+
+}
+
+using string::string_rule;
 
 }
 }
--- test/test_string.cpp
+++ test/test_string.cpp
@@ -8,19 +8,66 @@
 #include <hessian/parser.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
+#include <boost/format.hpp>
+#include <boost/algorithm/string/erase.hpp>
 #include "sample.hpp"
 
 namespace hessian {
 namespace parser {
 
+static string_t
+make_string_1024()
+{
+	boost::format f("%02d 456789012345678901234567890123456789012345678901234567890123\n");
+
+	string_t value;
+	for (std::size_t i = 0; i < 16; i++)
+		value += (f % i).str();
+	return value;
+}
+
+static string_t
+make_string_1023()
+{
+	return boost::algorithm::erase_tail_copy(make_string_1024(), 1);
+}
+
+static string_t
+make_string_65536()
+{
+	boost::format f("%03d 56789012345678901234567890123456789012345678901234567890123\n");
+
+	string_t value;
+	for (std::size_t j = 0; j < 2; j++)
+		for (std::size_t i = 0; i < 32 * 16; i++)
+			value += (f % i).str();
+	return value;
+}
+
+static string_t
+make_string_65535()
+{
+	return boost::algorithm::erase_tail_copy(make_string_65536(), 1);
+}
+
+const string_t s1023 = make_string_1023();
+const string_t s1024 = make_string_1024();
+const string_t s65535 = make_string_65535();
+const string_t s65536 = make_string_65536();
+
 const samples_t<string_t> samples
 {
 	{"\x00"s, ""s},
-	{"\x05hello"s, "hello"s},
-	{"\x01\xc3\x83"s, "\u00c3"s},
+	{"\x01""0"s, "0"s},
+	{"\x1f""0123456789012345678901234567890"s, "0123456789012345678901234567890"s},
+	{"\x30\x20""01234567890123456789012345678901"s, "01234567890123456789012345678901"s},
+	{"\x33\xff"s + s1023, s1023},
+	{"S\x04\x00"s + s1024, s1024},
+	{"R\x80\x00"s + string_t(s65535).insert(32768, "S\x7f\xff"s), s65535},
+	{"R\xff\xff"s + string_t(s65536).insert(65535, 1, '\x01'), s65536},
 
-	{"S\x00\x05hello"s, "hello"s},
-	{"s\x00\x07hello, \x05world"s, "hello, world"s},
+	{"\x01\xc3\x83"s, "\u00c3"s},
+	{"\x05""a""1""是""本""文"s, "a""1""是""本""文"s}
 };
 
 BOOST_AUTO_TEST_SUITE(test_string)
