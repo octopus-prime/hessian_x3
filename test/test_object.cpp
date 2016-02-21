7c76e5ea29a52259a8c7553fe73d2b77e17ad1b6
octopus-prime <mike_gresens@notebook>
2016-02-21 14:56:57 +0100

    Added test for object parser.

--- test/test_object.cpp
+++ test/test_object.cpp
@@ -20,6 +20,26 @@ const samples_t samples
 	{"\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""\x60""\x91"s, list_t{object_t{{"_value"s, 0}},object_t{{"_value"s, 1}}}},
 	{"\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""Q""\x91"s, list_t{object_t{{"_value"s, 0}},object_t{{"_value"s, 0}}}},
 	{"\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""\x60""\x90"s, list_t{object_t{{"_value"s, 0}},object_t{{"_value"s, 0}}}},
+	{"X""\xa1"
+			"C""\x00""\x90""\x60"
+			"C""\x00""\x90""\x61"
+			"C""\x00""\x90""\x62"
+			"C""\x00""\x90""\x63"
+			"C""\x00""\x90""\x64"
+			"C""\x00""\x90""\x65"
+			"C""\x00""\x90""\x66"
+			"C""\x00""\x90""\x67"
+			"C""\x00""\x90""\x68"
+			"C""\x00""\x90""\x69"
+			"C""\x00""\x90""\x6a"
+			"C""\x00""\x90""\x6b"
+			"C""\x00""\x90""\x6c"
+			"C""\x00""\x90""\x6d"
+			"C""\x00""\x90""\x6e"
+			"C""\x00""\x90""\x6f"
+			"C""\x00""\x90""O""\xa0"s,
+			list_t(17, object_t())
+	}
 };
 
 BOOST_AUTO_TEST_SUITE(test_object)
