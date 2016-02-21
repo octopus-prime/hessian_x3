951b437c6973c03ecac35d33b51cf48eb7bb8888
octopus-prime <mike_gresens@notebook>
2016-02-21 17:49:14 +0100

    Added exception

---
+++ include/hessian/exception.hpp
@@ -0,0 +1,16 @@
+/*
+ * exception.hpp
+ *
+ *  Created on: 21.02.2016
+ *      Author: mike_gresens
+ */
+
+#pragma once
+
+#include <exception>
+
+namespace hessian {
+
+class exception : public virtual std::exception {};
+
+}
