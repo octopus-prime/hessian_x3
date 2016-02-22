4ee4e3e5a3125649c34c32b493563e32aa3b7211
mgresens <mgresens@vjoon.com>
2016-02-22 10:05:12 +0100

    Using ll suffix for longs.

--- test/test_long.cpp
+++ test/test_long.cpp
@@ -15,24 +15,24 @@ namespace parser {
 
 const success_samples_t success_samples
 {
-	{"\xe0"s, 0l},
-	{"\xe1"s, 1l},
-	{"\xef"s, 15l},
-	{"\xd8"s, -8l},
-	{"\xf8\x10"s, 0x10l},
-	{"\xff\xff"s, 0x7ffl},
-	{"\xf7\xf7"s, -9l},
-	{"\xf0\x00"s, -0x800l},
-	{"\x3c\x08\x00"s, 0x800l},
-	{"\x3b\xf7\xff"s, -0x801l},
-	{"\x3f\xff\xff"s, 0x3ffffl},
-	{"\x38\x00\x00"s, -0x40000l},
-	{"Y\x00\x04\x00\x00"s, 0x40000l},
-	{"Y\xff\xfb\xff\xff"s, -0x40001l},
-	{"Y\x7f\xff\xff\xff"s, 0x7fffffffl},
-	{"Y\x80\x00\x00\x00"s, -0x80000000l},
-	{"L\x00\x00\x00\x00\x80\x00\x00\x00"s, 0x80000000l},
-	{"L\xff\xff\xff\xff\x7f\xff\xff\xff"s, -0x80000001l}
+	{"\xe0"s, 0ll},
+	{"\xe1"s, 1ll},
+	{"\xef"s, 15ll},
+	{"\xd8"s, -8ll},
+	{"\xf8\x10"s, 0x10ll},
+	{"\xff\xff"s, 0x7ffll},
+	{"\xf7\xf7"s, -9ll},
+	{"\xf0\x00"s, -0x800ll},
+	{"\x3c\x08\x00"s, 0x800ll},
+	{"\x3b\xf7\xff"s, -0x801ll},
+	{"\x3f\xff\xff"s, 0x3ffffll},
+	{"\x38\x00\x00"s, -0x40000ll},
+	{"Y\x00\x04\x00\x00"s, 0x40000ll},
+	{"Y\xff\xfb\xff\xff"s, -0x40001ll},
+	{"Y\x7f\xff\xff\xff"s, 0x7fffffffll},
+	{"Y\x80\x00\x00\x00"s, -0x80000000ll},
+	{"L\x00\x00\x00\x00\x80\x00\x00\x00"s, 0x80000000ll},
+	{"L\xff\xff\xff\xff\x7f\xff\xff\xff"s, -0x80000001ll}
 };
 
 BOOST_AUTO_TEST_SUITE(test_long)
