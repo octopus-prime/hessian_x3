b8398af69b9a85b84558d0713d74b4e3f2202dc5
mgresens <mgresens@vjoon.com>
2016-02-23 18:02:30 +0100

    Worked on example

--- example/Test.cpp
+++ example/Test.cpp
@@ -5,97 +5,123 @@
  *      Author: mike_gresens
  */
 
-#include <boost/spirit/home/x3.hpp>
-#include <boost/date_time/posix_time/posix_time_io.hpp>
-#include "null_parser.hpp"
-#include "bool_parser.hpp"
-#include "int_parser.hpp"
-#include "long_parser.hpp"
-#include "date_parser.hpp"
-#include "string_parser.hpp"
-//#include "list_parser.hpp"
-#include "map_parser.hpp"
-#include "value_parser.hpp"
+#include <hessian/parser.hpp>
+#include <boost/network.hpp>
+
 #include <iostream>
 #include <iomanip>
+#include <memory>
 
-namespace x3 = boost::spirit::x3;
 using namespace std::literals;
+namespace net = boost::network;
 
-void test_value()
+typedef net::http::client client_t;
+
+class service_base_t
 {
-	const std::string text("WL\x00\x00\x00\x00\x00\x00\x01\x2cNTFZ"s);
-	hessian::value_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::value_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
+protected:
+	virtual ~service_base_t() noexcept = default;
+
+public:
+	virtual int replyInt_0() = 0;
+	virtual int replyInt_1() = 0;
+	virtual long replyLong_0() = 0;
+	virtual long replyLong_1() = 0;
+};
+
+typedef std::shared_ptr<service_base_t> service_t;
+
+class service_impl_t : public virtual service_base_t
+{
+public:
+	service_impl_t(const std::string& host)
+	:
+		_url(host),
+		_client(client_t::options().cache_resolved(true))
+	{
+		_url << net::uri::path("/test/test2");
+	}
+
+	virtual int replyInt_0() override
+	{
+		const std::string content("H\x02\x00""C\x0a""replyInt_0\x90"s);
+		return boost::get<hessian::int_t>(call(content));
+	}
+
+	virtual int replyInt_1() override
+	{
+		const std::string content("H\x02\x00""C\x0a""replyInt_1\x90"s);
+		return boost::get<hessian::int_t>(call(content));
+	}
+
+	virtual long replyLong_0() override
+	{
+		const std::string content("H\x02\x00""C\x0b""replyLong_0\x90"s);
+		return boost::get<hessian::long_t>(call(content));
+	}
+
+	virtual long replyLong_1() override
+	{
+		const std::string content("H\x02\x00""C\x0b""replyLong_1\x90"s);
+		return boost::get<hessian::long_t>(call(content));
+	}
+
+protected:
+	hessian::value_t call(const std::string& content)
+	{
+		client_t::request request(_url);
+		request << net::header("Content-Length", std::to_string(content.size()));
+		request << net::body(content);
+
+		client_t::response response = _client.post(request);
+		std::stringstream stream(body(response));
+		char c;
+		stream >> c >> c >> c >> c;
+		return hessian::parse(stream);
+	}
+
+private:
+	net::uri::uri _url;
+	client_t _client;
+};
+
+void test2()
+{
+	const service_t service = std::make_shared<service_impl_t>("http://hessian.caucho.com");
+	std::cout << service->replyInt_0() << std::endl;
+	std::cout << service->replyInt_1() << std::endl;
+	std::cout << service->replyLong_0() << std::endl;
+	std::cout << service->replyLong_1() << std::endl;
 }
 /*
-void test_null()
+void foo(client_t& client)
 {
-	const std::string text("N"s);
-	hessian::null_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::null_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
+	const net::uri::uri url("http://hessian.caucho.com");
+	const std::string content("H\x02\x00""C\x0a""replyInt_0\x90"s);
+
+//	client_t::request request("http://hessian.caucho.com/test/test2");
+	client_t::request request(net::uri::uri(url) << net::uri::path("test/test2"));
+	request << net::header("Content-Length", std::to_string(content.size()));
+	request << net::body(content);
+
+	client_t::response response = client.post(request);
+	std::stringstream stream(body(response));
+	char c;
+	stream >> c >> c >> c >> c;
+	std::cout << hessian::parse(stream) << std::endl;
 }
 
-void test_bool()
+void test()
 {
-	const std::string text("T"s);
-	hessian::bool_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::bool_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
-}
+	client_t::options().cache_resolved(true);
 
-void test_int()
-{
-	const std::string text("I\x00\x00\x01\x2c"s);
-	hessian::int_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::int_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
-}
+	client_t client(options);
 
-void test_long()
-{
-	const std::string text("L\x00\x00\x00\x00\x00\x00\x01\x2c"s);
-	hessian::long_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::long_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
-}
-
-void test_date()
-{
-	const std::string text("d\x00\x00\x00\xd0\x4b\x92\x84\xb8"s);
-	hessian::date_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::date_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
-}
-
-void test_string()
-{
-	const std::string text("s\x00\x07hello, \x05world"s);
-	hessian::string_t attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::string_rule, attr) << std::endl;
-	std::cout << attr << std::endl;
-}
-
-void test_list()
-{
-	const std::string text("VTFFTz"s);
-	hessian::list_t<hessian::bool_t> attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::list_rule(hessian::parser::bool_rule), attr) << std::endl;
-	for (const auto element : attr)
-		std::cout << element << ',';
-	std::cout << std::endl;
-}
-
-void test_map()
-{
-	const std::string text("MTFFTz"s);
-	hessian::map_t<hessian::bool_t, hessian::bool_t> attr;
-	std::cout << x3::parse(text.begin(), text.end(), x3::eps > hessian::parser::map_rule(hessian::parser::bool_rule, hessian::parser::bool_rule), attr) << std::endl;
-	for (const auto& element : attr)
-		std::cout << element.first << "=" << element.second << ',';
-	std::cout << std::endl;
+	foo(client);
+	foo(client);
+	foo(client);
+	foo(client);
+	foo(client);
 }
 */
 int main()
@@ -104,19 +130,11 @@ int main()
 	{
 		std::cout << std::boolalpha;
 
-		test_value();
-//		test_null();
-//		test_bool();
-//		test_int();
-//		test_long();
-//		test_date();
-//		test_string();
-//		test_list();
-//		test_map();
+		test2();
 	}
-	catch (const x3::expectation_failure<std::string::const_iterator>& exception)
+	catch (const std::exception& exception)
 	{
-		std::cout << exception.what() << " " << exception.which() << " " << *exception.where() << std::endl;
+		std::cout << exception.what() << std::endl;
 	}
     return 0;
 }
