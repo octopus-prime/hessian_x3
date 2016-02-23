4d12f2a5eb61e44a5ff5279b01076e6ca9fb2194
octopus-prime <mike_gresens@notebook>
2016-02-23 08:19:56 +0100

    Long samples

--- test/test_long.cpp
+++ test/test_long.cpp
@@ -13,26 +13,31 @@
 namespace hessian {
 namespace parser {
 
+long_t operator"" _l (const unsigned long long l)
+{
+	return static_cast<long_t>(l);
+}
+
 const success_samples_t success_samples
 {
-	{"\xe0"s, 0ll},
-	{"\xe1"s, 1ll},
-	{"\xef"s, 15ll},
-	{"\xd8"s, -8ll},
-	{"\xf8\x10"s, 0x10ll},
-	{"\xff\xff"s, 0x7ffll},
-	{"\xf7\xf7"s, -9ll},
-	{"\xf0\x00"s, -0x800ll},
-	{"\x3c\x08\x00"s, 0x800ll},
-	{"\x3b\xf7\xff"s, -0x801ll},
-	{"\x3f\xff\xff"s, 0x3ffffll},
-	{"\x38\x00\x00"s, -0x40000ll},
-	{"Y\x00\x04\x00\x00"s, 0x40000ll},
-	{"Y\xff\xfb\xff\xff"s, -0x40001ll},
-	{"Y\x7f\xff\xff\xff"s, 0x7fffffffll},
-	{"Y\x80\x00\x00\x00"s, -0x80000000ll},
-	{"L\x00\x00\x00\x00\x80\x00\x00\x00"s, 0x80000000ll},
-	{"L\xff\xff\xff\xff\x7f\xff\xff\xff"s, -0x80000001ll}
+	{"\xe0"s, 0_l},
+	{"\xe1"s, 1_l},
+	{"\xef"s, 15_l},
+	{"\xd8"s, -8_l},
+	{"\xf8\x10"s, 0x10_l},
+	{"\xff\xff"s, 0x7ff_l},
+	{"\xf7\xf7"s, -9_l},
+	{"\xf0\x00"s, -0x800_l},
+	{"\x3c\x08\x00"s, 0x800_l},
+	{"\x3b\xf7\xff"s, -0x801_l},
+	{"\x3f\xff\xff"s, 0x3ffff_l},
+	{"\x38\x00\x00"s, -0x40000_l},
+	{"Y\x00\x04\x00\x00"s, 0x40000_l},
+	{"Y\xff\xfb\xff\xff"s, -0x40001_l},
+	{"Y\x7f\xff\xff\xff"s, 0x7fffffff_l},
+	{"Y\x80\x00\x00\x00"s, -0x80000000_l},
+	{"L\x00\x00\x00\x00\x80\x00\x00\x00"s, 0x80000000_l},
+	{"L\xff\xff\xff\xff\x7f\xff\xff\xff"s, -0x80000001_l}
 };
 
 BOOST_AUTO_TEST_SUITE(test_long)
