8f67bd28bac975291f87464dfd656f2f5dc41614
octopus-prime <mike_gresens@notebook>
2016-02-27 20:14:27 +0100

    Minor

--- source/client.cpp
+++ source/client.cpp
@@ -71,7 +71,7 @@ public:
 		const string_t call = generate(method, arguments);
 		const http::client::request request(url);
 		const http::client::response response = _client.post(request, generate(method, arguments));
-		const content_t content = hessian::parse(body(response));
+		const content_t content = parse(body(response));
 		return boost::apply_visitor(content_visitor(), content);
 	}
 
