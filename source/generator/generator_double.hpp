1f55098ab1076b9080e07ae3ef9c8b529b41ef89
octopus-prime <mike_gresens@notebook>
2016-02-28 15:44:16 +0100

    Worked on double generator.

--- source/generator/generator_double.hpp
+++ source/generator/generator_double.hpp
@@ -13,7 +13,47 @@ namespace generator {
 value_visitor::result_type
 value_visitor::operator()(const double_t& value)
 {
-	throw std::runtime_error("Not implemented."); //TODO
+	double buffer;
+	if (std::modf(value, &buffer) == 0.0)
+	{
+		if (value == 0.0)
+		{
+			push_back<std::int8_t>('\x5b');
+		}
+		else if (value == 1.0)
+		{
+			push_back<std::int8_t>('\x5c');
+		}
+		else if (value >= std::numeric_limits<std::int8_t>::min() && value <= std::numeric_limits<std::int8_t>::max())
+		{
+			push_back<std::int8_t>('\x5d');
+			push_back<std::int8_t>(value);
+		}
+		else if (value >= std::numeric_limits<std::int16_t>::min() && value <= std::numeric_limits<std::int16_t>::max())
+		{
+			push_back<std::int8_t>('\x5e');
+			push_back<std::int16_t>(value);
+		}
+		else
+		{
+			push_back<std::int8_t>('D');
+			push_back<std::int64_t>(*reinterpret_cast<const std::int64_t* const>(&value));
+		}
+	}
+	else
+	{
+		const double temp = 1000.0 * value;
+		if (std::modf(temp, &buffer) == 0.0 && temp >= std::numeric_limits<std::int32_t>::min() && temp <= std::numeric_limits<std::int32_t>::max())
+		{
+			push_back<std::int8_t>('\x5f');
+			push_back<std::int32_t>(temp);
+		}
+		else
+		{
+			push_back<std::int8_t>('D');
+			push_back<std::int64_t>(*reinterpret_cast<const std::int64_t* const>(&value));
+		}
+	}
 }
 
 }
