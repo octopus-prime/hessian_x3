639c3913164011bcfeb47a313c6bc87d3a95c74d
octopus-prime <mike_gresens@notebook>
2016-02-28 14:41:33 +0100

    Worked on int generator.

--- source/generator/generator_int.hpp
+++ source/generator/generator_int.hpp
@@ -13,7 +13,25 @@ namespace generator {
 value_visitor::result_type
 value_visitor::operator()(const int_t& value)
 {
-	push_back<std::int8_t>('\x90' + value); //TODO
+	if (value >= -16 && value <= 47)
+	{
+		push_back<std::int8_t>('\x90' + value);
+	}
+	else if (value >= -0x800 && value <= 0x7ff)
+	{
+		push_back<std::int8_t>('\xc8' + (value >> 8));
+		push_back<std::int8_t>(value & 0xff);
+	}
+	else if (value >= -0x40000 && value <= 0x3ffff)
+	{
+		push_back<std::int8_t>('\xd4' + (value >> 16));
+		push_back<std::int16_t>(value & 0xffff);
+	}
+	else
+	{
+		push_back<std::int8_t>('I');
+		push_back<std::int32_t>(value);
+	}
 }
 
 }
