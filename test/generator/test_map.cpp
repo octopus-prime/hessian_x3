41715125d3d35d136129ecd686cdee169a44902f
octopus-prime <mike_gresens@notebook>
2016-02-28 18:19:02 +0100

    Added test for map generator.

---
+++ test/generator/test_map.cpp
@@ -0,0 +1,36 @@
+/*
+ * test_map.cpp
+ *
+ *  Created on: 28.02.2016
+ *      Author: mike_gresens
+ */
+
+#include <hessian/generator.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include "../sample.hpp"
+
+namespace hessian {
+namespace generator {
+
+const success_samples_t success_samples
+{
+	{"HZ"s, map_t{}},
+	{"H""\x01""a""\x90""Z"s, map_t{{"a"s, 0}}},
+	{"H""\x91""\x01""b""\x90""\x01""a""Z"s, map_t{{0, "a"s}, {1, "b"s}}},
+	{"H""\x79""\x01""a""\x90""Z"s, map_t{{list_t{"a"s}, 0}}},
+};
+
+BOOST_AUTO_TEST_SUITE(test_map)
+
+BOOST_DATA_TEST_CASE(test_success, success_samples, sample)
+{
+	string_t call;
+	BOOST_REQUIRE_NO_THROW(call = generate("test"s, {sample.second}));
+	BOOST_CHECK_EQUAL(call, "H\x02\x00""C\x04""test\x91"s + sample.first);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
+}
