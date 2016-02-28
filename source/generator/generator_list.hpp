3fc18817b6d3fa99ab5f25bab358f7c6937eb844
octopus-prime <mike_gresens@notebook>
2016-02-28 16:22:58 +0100

    Worked on list generator.

--- source/generator/generator_list.hpp
+++ source/generator/generator_list.hpp
@@ -13,12 +13,19 @@ namespace generator {
 value_visitor::result_type
 value_visitor::operator()(const list_t& value)
 {
-	if (value.empty()) //TODO
-		return;
-
-	push_back<std::int8_t>('\x78' + value.size());
-	for (const value_t& element : value)
-		boost::apply_visitor(*this, element);
+	if (value.size() <= 7)
+	{
+		push_back<std::int8_t>('\x78' + value.size());
+		for (const value_t& element : value)
+			boost::apply_visitor(*this, element);
+	}
+	else
+	{
+		push_back<std::int8_t>('W');
+		for (const value_t& element : value)
+			boost::apply_visitor(*this, element);
+		push_back<std::int8_t>('Z');
+	}
 }
 
 }
