f23757de1a3259bc5a3b5a52804689bdcfcca706
octopus-prime <mike_gresens@notebook>
2016-02-28 19:03:52 +0100

    Added test for object generator.

---
+++ test/generator/test_object.cpp
@@ -0,0 +1,57 @@
+/*
+ * test_object.cpp
+ *
+ *  Created on: 28.02.2016
+ *      Author: mike_gresens
+ */
+
+#include <hessian/generator.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include "../sample.hpp"
+
+namespace hessian {
+namespace generator {
+
+const success_samples_t success_samples
+{
+//	{"C""\x00""\x90""\x60"s, object_t{}},
+//	{"C""\x00""\x91""\x06""_value""\x60""\x90"s, object_t{{"_value"s, 0}}},
+//	{"\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""\x60""\x91"s, list_t{object_t{{"_value"s, 0}},object_t{{"_value"s, 1}}}},
+//	{"\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""Q""\x91"s, list_t{object_t{{"_value"s, 0}},object_t{{"_value"s, 0}}}},
+//	{"\x7a""C""\x00""\x91""\x06""_value""\x60""\x90""\x60""\x90"s, list_t{object_t{{"_value"s, 0}},object_t{{"_value"s, 0}}}},
+//	{"X""\xa1"
+//			"C""\x00""\x90""\x60"
+//			"C""\x00""\x90""\x61"
+//			"C""\x00""\x90""\x62"
+//			"C""\x00""\x90""\x63"
+//			"C""\x00""\x90""\x64"
+//			"C""\x00""\x90""\x65"
+//			"C""\x00""\x90""\x66"
+//			"C""\x00""\x90""\x67"
+//			"C""\x00""\x90""\x68"
+//			"C""\x00""\x90""\x69"
+//			"C""\x00""\x90""\x6a"
+//			"C""\x00""\x90""\x6b"
+//			"C""\x00""\x90""\x6c"
+//			"C""\x00""\x90""\x6d"
+//			"C""\x00""\x90""\x6e"
+//			"C""\x00""\x90""\x6f"
+//			"C""\x00""\x90""O""\xa0"s,
+//			list_t(17, object_t())
+//	}
+};
+
+BOOST_AUTO_TEST_SUITE(test_object)
+
+BOOST_DATA_TEST_CASE(test_success, success_samples, sample)
+{
+	string_t call;
+	BOOST_REQUIRE_NO_THROW(call = generate("test"s, {sample.second}));
+	BOOST_CHECK_EQUAL(call, "H\x02\x00""C\x04""test\x91"s + sample.first);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
+}
