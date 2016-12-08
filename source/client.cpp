58661ab09c7e350382a098481a7b502a1d52b7ef
octopus-prime <mike.gresens@gmail.com>
2016-12-08 13:08:03 +0100

    Using curl share interface. To use the same cookie database, DNS cache, TLS session cache.

--- source/client.cpp
+++ source/client.cpp
@@ -9,6 +9,8 @@
 #include <hessian/parser.hpp>
 #include <hessian/generator.hpp>
 #include <curl/curl.h>
+#include <array>
+#include <boost/thread/shared_mutex.hpp>
 
 using namespace std::literals;
 
@@ -54,80 +56,201 @@ struct content_visitor : boost::static_visitor<const value_t&>
 
 class client_impl : public virtual client_base
 {
+	class post
+	{
+	public:
+		post(const std::shared_ptr<CURLSH>& handle)
+		:
+			_error{{0}},
+			_handle(curl_easy_init(), curl_easy_cleanup)
+		{
+			set_option(CURLOPT_SHARE, handle.get());
+			set_option(CURLOPT_ERRORBUFFER, _error.data());
+			set_option(CURLOPT_POST, 1L);
+			set_option(CURLOPT_WRITEFUNCTION, write);
+			set_option(CURLOPT_USERAGENT, "libhessian/1.0");
+			set_option(CURLOPT_HTTPHEADER, HEADER.get());
+			set_option(CURLOPT_VERBOSE, 0L);
+		}
+
+		std::string
+		operator()(const string_t& url, const string_t& call) const
+		{
+			std::ostringstream output;
+
+			set_option(CURLOPT_URL, url.c_str());
+			set_option(CURLOPT_POSTFIELDS, call.data());
+			set_option(CURLOPT_POSTFIELDSIZE, call.size());
+			set_option(CURLOPT_WRITEDATA, &output);
+
+			const auto result = perform();
+
+			if (result != 200)
+				throw std::runtime_error(std::to_string(result) + " " + __func__);
+
+			return output.str();
+		}
+
+	protected:
+		std::string
+		get_error(const CURLcode code) const
+		{
+			return _error.front() != 0 ? _error.data() : curl_easy_strerror(code);
+		}
+
+		template <typename T>
+		void
+		set_option(const CURLoption key, const T value) const
+		{
+			const CURLcode code = curl_easy_setopt(_handle.get(), key, value);
+			if (code != CURLE_OK)
+				throw std::runtime_error(get_error(code) + " " + __func__);
+		}
+
+		long
+		perform() const
+		{
+			CURLcode code = curl_easy_perform(_handle.get());
+
+			if (code != CURLE_OK)
+				throw std::runtime_error(get_error(code) + " " + __func__);
+
+			long result;
+
+			code = curl_easy_getinfo(_handle.get(), CURLINFO_RESPONSE_CODE, &result);
+			if (code != CURLE_OK)
+				throw std::runtime_error(get_error(code) + " " + __func__);
+
+			return result;
+		}
+
+		static size_t
+		write(char *buffer, size_t size, size_t nitems, void *outstream)
+		{
+			const size_t length = size * nitems;
+			reinterpret_cast<std::ostream*>(outstream)->write(buffer, length);
+			return length;
+		}
+
+	private:
+		std::array<char, CURL_ERROR_SIZE> _error;
+		std::shared_ptr<CURL> _handle;
+
+		static const std::shared_ptr<curl_slist> HEADER;
+};
+
 public:
 	client_impl(const std::string& host)
 	:
 		_url(host),
-		_client(curl_easy_init(), curl_easy_cleanup),
-		_header(curl_slist_append(nullptr, "Content-Type: x-application/hessian"), curl_slist_free_all)
+		_mutex_cookie(),
+		_mutex_dns(),
+		_mutex_ssl(),
+		_mutex_connect(),
+		_handle(curl_share_init(), curl_share_cleanup)
 	{
-//		set_option(CURLOPT_VERBOSE, 1L);
-		set_option(CURLOPT_POST, 1L);
-		set_option(CURLOPT_WRITEFUNCTION, write);
-		set_option(CURLOPT_USERAGENT, "libhessian/1.0");
-		set_option(CURLOPT_HTTPHEADER, _header.get());
+		set_option(CURLSHOPT_SHARE, CURL_LOCK_DATA_COOKIE);
+		set_option(CURLSHOPT_SHARE, CURL_LOCK_DATA_DNS);
+		set_option(CURLSHOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
+		set_option(CURLSHOPT_SHARE, CURL_LOCK_DATA_CONNECT);
+		set_option(CURLSHOPT_LOCKFUNC, lock);
+		set_option(CURLSHOPT_UNLOCKFUNC, unlock);
+		set_option(CURLSHOPT_USERDATA, this);
 	}
 
 	virtual value_t call(const string_t& service, const string_t& method, const list_t& arguments) override
 	{
 		const string_t url = _url + service;
 		const string_t call = generate(method, arguments);
-		std::ostringstream output;
 
-		set_option(CURLOPT_URL, url.c_str());
-		set_option(CURLOPT_POSTFIELDS, call.data());
-		set_option(CURLOPT_POSTFIELDSIZE, call.size());
-		set_option(CURLOPT_WRITEDATA, &output);
+		const post request(_handle);
+		const std::string result = request(url, call);
 
-		const auto result = perform();
-
-		if (result != 200)
-			throw std::runtime_error(std::to_string(result) + " " + __func__);
-
-		const content_t content = parse(output.str());
+		const content_t content = parse(result);
 		return boost::apply_visitor(content_visitor(), content);
 	}
 
 protected:
+	std::string
+	get_error(const CURLSHcode code) const
+	{
+		return curl_share_strerror(code);
+	}
+
 	template <typename T>
 	void
-	set_option(const CURLoption key, const T value) const
+	set_option(const CURLSHoption key, const T value) const
 	{
-		const CURLcode code = curl_easy_setopt(_client.get(), key, value);
-		if (code != CURLE_OK)
-			throw std::runtime_error(std::to_string(code) + " " + __func__);
+		const CURLSHcode code = curl_share_setopt(_handle.get(), key, value);
+		if (code != CURLSHE_OK)
+			throw std::runtime_error(get_error(code) + " " + __func__);
 	}
 
-	long
-	perform() const
+	static void
+	lock(CURL *handle, curl_lock_data data, curl_lock_access access, void *userptr)
 	{
-		CURLcode code = curl_easy_perform(_client.get());
-
-		if (code != CURLE_OK)
-			throw std::runtime_error(std::to_string(code) + " " + __func__);
-
-		long result;
-
-		code = curl_easy_getinfo(_client.get(), CURLINFO_RESPONSE_CODE, &result);
-		if (code != CURLE_OK)
-			throw std::runtime_error(std::to_string(code) + " " + __func__);
-
-		return result;
+		client_impl* client = reinterpret_cast<client_impl*>(userptr);
+		switch (data)
+		{
+		case CURL_LOCK_DATA_COOKIE:
+			access == CURL_LOCK_ACCESS_SHARED
+				? client->_mutex_cookie.lock_shared()
+				: client->_mutex_cookie.lock();
+			break;
+		case CURL_LOCK_DATA_DNS:
+			access == CURL_LOCK_ACCESS_SHARED
+				? client->_mutex_dns.lock_shared()
+				: client->_mutex_dns.lock();
+			break;
+		case CURL_LOCK_DATA_SSL_SESSION:
+			access == CURL_LOCK_ACCESS_SHARED
+				? client->_mutex_ssl.lock_shared()
+				: client->_mutex_ssl.lock();
+			break;
+		case CURL_LOCK_DATA_CONNECT:
+			access == CURL_LOCK_ACCESS_SHARED
+				? client->_mutex_connect.lock_shared()
+				: client->_mutex_connect.lock();
+			break;
+		default:
+			break;
+		}
 	}
 
-	static size_t
-	write(char *buffer, size_t size, size_t nitems, void *outstream)
+	static void
+	unlock(CURL *handle, curl_lock_data data, void *userptr)
 	{
-		const size_t length = size * nitems;
-		reinterpret_cast<std::ostream*>(outstream)->write(buffer, length);
-		return length;
+		client_impl* client = reinterpret_cast<client_impl*>(userptr);
+		switch (data)
+		{
+		case CURL_LOCK_DATA_COOKIE:
+			client->_mutex_cookie.unlock();
+			break;
+		case CURL_LOCK_DATA_DNS:
+			client->_mutex_dns.unlock();
+			break;
+		case CURL_LOCK_DATA_SSL_SESSION:
+			client->_mutex_ssl.unlock();
+			break;
+		case CURL_LOCK_DATA_CONNECT:
+			client->_mutex_connect.unlock();
+			break;
+		default:
+			break;
+		}
 	}
 
 private:
 	std::string _url;
-	std::shared_ptr<CURL> _client;
-	std::shared_ptr<curl_slist> _header;
+	boost::shared_mutex _mutex_cookie;
+	boost::shared_mutex _mutex_dns;
+	boost::shared_mutex _mutex_ssl;
+	boost::shared_mutex _mutex_connect;
+	std::shared_ptr<CURLSH> _handle;
 };
+
+const std::shared_ptr<curl_slist>
+client_impl::post::HEADER(curl_slist_append(nullptr, "Content-Type: x-application/hessian"), curl_slist_free_all);
 
 client_t
 make_client(const std::string& host)
@@ -136,4 +259,3 @@ make_client(const std::string& host)
 }
 
 }
-
