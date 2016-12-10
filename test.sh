d2db4b6050babc5ec0ce587ce6e136cd55a200c3
octopus-prime <mike.gresens@gmail.com>
2016-12-10 13:37:45 +0100

    Testing

---
+++ test.sh
@@ -0,0 +1,7 @@
+docker rm -f hessian_x3 &>/dev/null || true
+docker run -d -it --name hessian_x3 --privileged octopusprime/test
+docker exec hessian_x3 mkdir hessian_x3
+docker cp . hessian_x3:/hessian_x3
+docker exec hessian_x3 bash -c "
+	cd hessian_x3; 
+	bjam toolset=gcc release -j2"
