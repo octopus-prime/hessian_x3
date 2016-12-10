d41e58ffc12dbc7cfa630f7fc7112b82b01850b6
octopus-prime <mike.gresens@gmail.com>
2016-12-10 15:30:42 +0100

    Coverage

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ docker exec hessian_x3 mkdir hessian_x3
 docker cp . hessian_x3:/hessian_x3
 docker exec hessian_x3 bash -c "
 	cd hessian_x3; 
-	bjam toolset=$1 release -j2"
+	bjam toolset=$1 debug -j$2
+"
+docker cp hessian_x3:/hessian_x3/bin .
